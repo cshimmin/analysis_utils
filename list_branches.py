ef169c7865090063302816868e406a3260bb148e
Chase Shimmin <cshimmin@cern.ch>
2013-07-16 18:59:35 +0200

    added list_branches utility

---
+++ list_branches.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import argparse
+import ROOT as r
+r.PyConfig.IgnoreCommandLineOptions = True
+
+TREE_NAME = 'physics'
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="List branches of a tree in a rootfile")
+	parser.add_argument('root_file', type=str,
+			help='Input root file')
+	parser.add_argument('--tree', type=str, nargs=1, default="physics",
+			help='The name of the TTree within the root file')
+	args = parser.parse_args()
+
+	f = r.TFile(args.root_file)
+	t = f.Get(args.tree)
+
+	for b in t.GetListOfBranches():
+		print b.GetName()
+
