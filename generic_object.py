7f58cfe7ad6c2d7e1d1cdb32a9a3eb9ad96ed328
Chase Shimmin <cshimmin@uci.edu>
2013-09-04 12:08:08 +0200

    added serialize method for generic_objects

--- generic_object.py
+++ generic_object.py
@@ -16,6 +16,12 @@ class generic_obj:
 		answer = getattr(self.tree, '%s_%s'%(self.prefix,attr))[self.idx]
 		setattr(self, attr, answer)
 		return answer
+	def serialize(self):
+		branches = [b.GetName()[len(self.prefix)+1:] for b in self.tree.GetListOfBranches() if b.GetName().startswith(self.prefix) and b.GetName() != '%s_n'%self.prefix]
+		#print branches
+		data = ( (b, getattr(self, b)) for b in branches)
+		datasane = filter(lambda d: not 'vector' in str(d[1].__class__), data)
+		return dict(datasane)
 
 # conveneince methods to generate/load lists of D3PD objects based on their branch prefix
 def fetch_objects(tree, prefix):
