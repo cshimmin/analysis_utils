01d9f546ee1abd571889d96b2e813a011143d105
Chase Shimmin <cshimmin@uci.edu>
2013-10-06 21:09:36 -0700

    initial commit of pytree utility -- make it easy to bind branches

---
+++ pytree.py
@@ -0,0 +1,65 @@
+import ROOT as r
+import numpy as np
+
+'''
+simple tree definitions using python dictionaries
+'''
+
+# example tree definition
+treedef = {
+	'leading_ph_pt': float,
+	'leading_ph_eta': float,
+	'leading_ph_phi': float,
+	'leading_ph_E': float,
+	'leading_ph_idx': int,
+	'jet_corr_pt': [float],
+	'jet_corr_eta': [float],
+	'jet_corr_phi': [float],
+	'jet_corr_E': [float],
+	'jet_corr_M': [float],
+	'met_wet': [[float]],
+	'met_wex': [[float]],
+	'met_wey': [[float]],
+	}
+
+
+def setup_tree(name, treedef):
+	t = r.TTree(name, name)
+
+	bind_scalar(t, 'valid', int)
+
+	branch_names = sorted(treedef.keys())
+	for bname in branch_names:
+		btype = treedef[bname]
+
+		if not type(btype) == list:
+			bind_scalar(t, bname, btype)
+		elif len(btype) and type(btype[0]) == list:
+			if len(btype[0]):
+				bind_vector2(t, bname, btype[0][0])
+			else:
+				bind_vector2(t, bname, float)
+		elif len(btype):
+			bind_vector(t, bname, btype[0])
+		else:
+			bind_vector(t, bname, float)
+	
+	return t
+
+typenames_long = { float: 'double', int: 'int' }
+typenames_short = { float: 'D', int: 'I' }
+
+def bind_scalar(t, bname, btype):
+	v = np.array([0], dtype=btype)
+	t.Branch(bname, v, '%s/%s' % (bname, typenames_short[btype]))
+	setattr(t, bname, v)
+
+def bind_vector(t, bname, btype):
+	v = r.vector(typenames_long[btype])()
+	t.Branch(bname, 'vector<%s>'%typenames_long[btype], v)
+	setattr(t, bname, v)
+
+def bind_vector2(t, bname, btype):
+	v = r.vector('vector<%s>'%typenames_long[btype])()
+	t.Branch(bname, 'vector<vector<%s> >'%typenames_long[btype], v)
+	setattr(t, bname, v)
