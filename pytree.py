c202000fe57fdc7b22095ab3b5b90a0736b89873
Chase Shimmin <cshimmin@uci.edu>
2014-02-10 22:31:19 -0800

    pytree: automatic branch type detection
    Specifying the branch type is now optional in write_branch().
    A little more work needs to be done for nested-vector support.
    
    Also removed some old crufty example code.

--- pytree.py
+++ pytree.py
@@ -1,58 +1,14 @@
 import ROOT as r
 import numpy as np
 
-'''
-simple tree definitions using python dictionaries
-
-to create a new tree with some branches, do:
-    t = setup_tree('awesometree', treedef)
-
-see below for example tree definition
-
-N.B.: due to the fact that python's primitive types are all pass-by-value,
-for scalar-type branches, you'll have to use zero-indexing to write values:
-    t = setup_tree('physics', {'some_scalar': float})
-    t.some_scalar[0] = 3.14
-    t.Fill()
-'''
-
-# example tree definition
-treedef = {
-    'leading_ph_pt': float,
-    'leading_ph_eta': float,
-    'leading_ph_phi': float,
-    'leading_ph_E': float,
-    'leading_ph_idx': int,
-    'jet_corr_pt': [float],
-    'jet_corr_eta': [float],
-    'jet_corr_phi': [float],
-    'jet_corr_E': [float],
-    'jet_corr_M': [float],
-    'met_wet': [[float]],
-    'met_wex': [[float]],
-    'met_wey': [[float]],
-}
-
-
-def setup_tree(name, treedef):
-    t = r.TTree(name, name)
-
-    bind_scalar(t, 'valid', int)
-
-    branch_names = sorted(treedef.keys())
-    for bname in branch_names:
-        btype = treedef[bname]
-        bind_any(t, bname, btype)
-
-    return t
-
-
 class PyTree(r.TTree):
 
     def __init__(self, *args, **kwargs):
         r.TTree.__init__(self, *args)
 
         self.__branch_cache = {}
+
+        self.verbose = kwargs.get('verbose', False)
 
     def reset(self):
         for v, btype in self.__branch_cache.values():
@@ -67,30 +23,83 @@ class PyTree(r.TTree):
                 # a scalar value. set it to zero.
                 v[0] = 0
 
-    def write_branch(self, value, bname, btype):
+    def write_branch(self, value, bname, btype=None):
         try:
             v, _ = self.__branch_cache[bname]
         except KeyError:
-            # oops haven't made this branch yet.
+            if btype == None:
+                try:
+                    btype = infer_btype(value)
+                except EmptyValue:
+                    # the value was iterable, but empty. just move on
+                    # for now, since we cannot infer the type. the
+                    # next call may have a nonempty value, and we will
+                    # create the branch then.
+                    return
+
+            # oops, haven't made this branch yet. let's do that now:
             v = bind_and_backfill(self, bname, btype)
             self.__branch_cache[bname] = (v, btype)
 
+            if self.verbose:
+                print "pytree: Created branch '%s' (type: %s)" % (bname, get_typeclass(btype))
+
+
         # now we have a pointer to the branch memory.
         # write the value depending on the type
-        if type(btype) == list:
+        if btype in (int, float, bool):
+            # just a scalar type; these are managed by numpy.ndarray's:
+            v[0] = value
+        elif type(btype) == list:
             # some vector type
             if btype[0] == list:
                 # TODO!
-                raise TypeError('Unsupported type: %s' % btype)
-            # okay, just a normal vector.
+                raise TypeError('pytree: unsupported type (nested vectors coming soon...)' % btype)
+            # just a normal (1-d) vector.
             # copy the input array with push_back
             map(v.push_back, value)
-        else:
-            # some sclar type
-            v[0] = value
 
-typenames_long = {float: 'double', int: 'int'}
-typenames_short = {float: 'D', int: 'I'}
+
+typenames_long = {float: 'double', int: 'int', bool: 'bool'}
+typenames_short = {float: 'D', int: 'I', bool: 'O'}
+
+''' Exception to represent type inference failure due to
+    an empty iterable. '''
+class EmptyValue(TypeError):
+    pass
+
+''' Try to invert the appropriate branch type
+    for the given value '''
+def infer_btype(val):
+    t = type(val)
+
+    if t == list:
+        if len(val) == 0:
+            raise EmptyValue('pytree: cannot infer type from empty iterable.')
+        return [infer_btype(val[0])]
+    elif t == tuple:
+        return tuple( infer_btype(v) for v in val )
+    elif t in (int, float, bool):
+        return t
+    elif t == np.ndarray:
+        dt = {'i': int, 'f': float}[val.dtype.kind]
+        def nest(n):
+            return dt if n==0 else [nest(n-1)]
+        return nest(len(val.shape))
+    else:
+        raise TypeError('pytree: unsupported type.')
+
+def get_typeclass(btype):
+    if btype in (int, float, bool):
+        return typenames_long[btype]
+
+    t = type(btype)
+    if t == list:
+        return 'vector< %s >' % get_typeclass(btype[0])
+    elif t == tuple:
+        raise TypeError('pytree: unsupported type (tuples are coming soon...)')
+    else:
+        raise TypeError('pytree: unsupported type.')
 
 
 def bind_and_backfill(t, bname, btype):
@@ -106,41 +115,33 @@ def bind_and_backfill(t, bname, btype):
     return v
 
 
-def bind_any(t, bname, btype):
-    if not type(btype) == list:
-        v = bind_scalar(t, bname, btype)
-    elif len(btype) and type(btype[0]) == list:
-        if len(btype[0]):
-            v = bind_vector2(t, bname, btype[0][0])
-        else:
-            v = bind_vector2(t, bname, float)
-    elif len(btype):
-        v = bind_vector(t, bname, btype[0])
+def bind_any(tree, bname, btype):
+    if btype in (int, float, bool):
+        return bind_scalar(tree, bname, btype)
+    
+    t = type(btype)
+    if t == list:
+        if len(btype) == 0:
+            raise TypeError('pytree: unsupported type []')
+
+        v = bind_vector(tree, bname, btype)
     else:
-        v = bind_vector(t, bname, float)
+        raise TypeError('pytree: unsupported type')
 
     return v
 
 
-def bind_scalar(t, bname, btype):
+def bind_scalar(tree, bname, btype):
     v = np.array([0], dtype=btype)
-    b = t.Branch(bname, v, '%s/%s' % (bname, typenames_short[btype]))
-    setattr(t, bname, v)
+    b = tree.Branch(bname, v, '%s/%s' % (bname, typenames_short[btype]))
+    setattr(tree, bname, v)
 
     return v
 
 
-def bind_vector(t, bname, btype):
-    v = r.vector(typenames_long[btype])()
-    b = t.Branch(bname, 'vector<%s>' % typenames_long[btype], v)
-    setattr(t, bname, v)
-
-    return v
-
-
-def bind_vector2(t, bname, btype):
-    v = r.vector('vector<%s>' % typenames_long[btype])()
-    b = t.Branch(bname, 'vector<vector<%s> >' % typenames_long[btype], v)
-    setattr(t, bname, v)
+def bind_vector(tree, bname, btype):
+    v = r.vector( get_typeclass(btype[0]) )()
+    b = tree.Branch(bname, get_typeclass(btype), v)
+    setattr(tree, bname, v)
 
     return v
