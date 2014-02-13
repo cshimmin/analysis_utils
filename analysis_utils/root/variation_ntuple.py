fd90632f8aaede3cde167f009f01e2da8beb28e5
Chase Shimmin <cshimmin@uci.edu>
2014-02-13 02:22:04 -0800

    Add populate_ntuple() method to abstract away some of the TTree details

--- analysis_utils/root/variation_ntuple.py
+++ analysis_utils/root/variation_ntuple.py
@@ -14,10 +14,8 @@ class AnalysisVariationNTuple(variation.AnalysisVariation):
 
         # create the output ntuple. if not output name is supplied,
         # use the descriptive name, replacing spaces w/ underscores.
-        try:
-            output_name = kwargs['output_name']
-        except KeyError:
-            output_name = '_'.join(self._name.split())
+        output_name = kwargs.get('output_name', '_'.join(self._name.split()))
+
         self._output_tree = PyTree(output_name, self._name)
 
     def get_ntuple(self):
@@ -27,3 +25,27 @@ class AnalysisVariationNTuple(variation.AnalysisVariation):
         variation.AnalysisVariation.reset(self)
 
         self._output_tree.reset()
+
+    '''
+     The user should override this method, writing out whatever information
+     they want to include in the output ntuple. A reference to the underlying
+     PyTree is passed as an argument for convenience.
+    '''
+    def populate_ntuple(self, ntuple):
+        print "WARNING! Base class populate_ntuple() invoked for variation `%s`!" % self._name
+
+    ''' This entry was accepted! Set valid flag and fill! '''
+    def accept_entry(self):
+        t = self._output_tree
+        self.populate_ntuple(t)
+        t.write_branch(True, '_valid')
+        t.Fill()
+
+    '''
+     If this instance is rejected, but the entry was accepted for the
+     combined analysis, we should write out an empty entry.
+    '''
+    def reject_entry(self):
+        t = self._output_tree
+        t.write_branch(False, '_valid')
+        t.Fill()
