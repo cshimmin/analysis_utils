0140fcf1b4b2c611e5b3225f32a72380fbf14e5e
Chase Shimmin <cshimmin@gmail.com>
2015-01-14 12:49:00 -0800

    add setup.py script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='analysis_utils',
+      version='0.1.0',
+      description='Basic python helpers for ROOT-based analyses.',
+      url='https://github.com/cshimmin/analysis_utils',
+      author='Chase Shimmin',
+      author_email='cshimmin@uci.edu',
+      packages=find_packages(),
+      zip_safe=False)
